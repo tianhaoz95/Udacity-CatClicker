cd2157af3ac0ee7d86ec8d9ca1ff1969194263e6
tianhaoz <tianhaoz@umich.edu>
2017-03-28 11:57:32 -0400

    spec change 2

--- script/main.js
+++ script/main.js
@@ -1,21 +1,43 @@
-var count1 = 0;
-var count2 = 0;
-var cat_name_1 = "Alice";
-var cat_name_2 = "Thea";
+
+var kitty_list = [
+  'Thea',
+  'Alice',
+  'Sofia',
+  'Shakira',
+  'Hailee',
+];
+
+var srcs = [
+  'img/cat1.jpg',
+  'img/cat2.jpg',
+  'img/cat3.jpg',
+  'img/cat4.jpg',
+  'img/cat5.jpg',
+];
+
+var counters = [0, 0, 0, 0, 0];
 
 $(document).ready(function() {
-  $('#count1').text(count1);
-  $('#count2').text(count2);
-  $('#catName1').text(cat_name_1);
-  $('#catName2').text(cat_name_2);
-});
 
-$('#cat_img1').click(function() {
-  count1 = count1 + 1;
-  $('#count1').text(count1);
-});
+  for (var i = 0; i < kitty_list.length; i++) {
+    var kitty_name = $('<p></p>').text(kitty_list[i]);
+    var kitty_entry = $('<a></a>').html(kitty_name).click((function(idx) {
+      return function() {
+        $('#kitty_detail').empty();
+        var kitty_img = $('<img>', {height:300 , src:srcs[idx]});
+        var click_count = $('<p></p>', {id:'kitty_counter'}).text(counters[idx]);
+        var kitty_name = $('<p></p>').text(kitty_list[idx]);
+        var kitty_click = $('<a></a>').html(kitty_img).click(function() {
+          var old = counters[idx];
+          counters[idx] = old + 1;
+          $('#kitty_counter').text(counters[idx]);
+        });
+        $('#kitty_detail').append(kitty_name);
+        $('#kitty_detail').append(click_count);
+        $('#kitty_detail').append(kitty_click);
+      };
+    })(i));
+    $('#kitty_list').append(kitty_entry);
+  }
 
-$('#cat_img2').click(function() {
-  count2 = count2 + 1;
-  $('#count2').text(count2);
 });
